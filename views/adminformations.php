86f287114bb8adb4ca7fdc768405d7d453f0292e
elgeogeo <geoffroy.rouaix@hotmail.Com>
2024-06-12 17:57:43 +0200

    delete course ok

--- views/adminformations.php
+++ views/adminformations.php
@@ -354,7 +354,7 @@ foreach ($courses as $course) {
                 }
                 // var_dump($countenroll);
                 $content .= '
-                    <a data-toggle="tooltip" data-placement="top" title="Supprimer la formation" onclick="deleteCourse(\'' . new moodle_url('/course/delete.php') . '?id=' . $course->id . '\', \'group\', \''.$possible .'\')">
+                    <a data-toggle="tooltip" data-placement="top" title="Supprimer la formation" onclick="deleteCourse(\'' . new moodle_url('/theme/remui/views/deletecourse.php') . '?id=' . $course->id . '\', \'group\', \''.$possible .'\')">
                         <svg class="iconsvg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                         </svg>
