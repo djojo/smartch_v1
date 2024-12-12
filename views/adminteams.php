81de353151d17eff2ad253f267cfd2e07748698c
elgeogeo <geoffroy.rouaix@hotmail.Com>
2024-12-12 17:47:22 +0100

    trie par groupe 4

--- views/adminteams.php
+++ views/adminteams.php
@@ -107,8 +107,8 @@ echo $OUTPUT->header();
 //   src="https://code.jquery.com/jquery-3.7.1.min.js"
 //   integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
 //   crossorigin="anonymous"></script>';
-// echo '<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-// <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>';
+echo '<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>';
 
 
 // echo html_writer::start_div('container');
