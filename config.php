f4d964507cb6e4fdf0e8fa4545f0b4bc0f4ca24b
Maunick Mayo <mmayo@smartchlab.fr>
2026-04-20 22:27:08 +0200

    fix: scss preset fallback to precompiled css (revert sheets)

--- config.php
+++ config.php
@@ -27,11 +27,11 @@ defined('MOODLE_INTERNAL') || die();
 require_once(__DIR__ . '/lib.php');
 
 $THEME->name = 'remui';
-$THEME->sheets = ['remui'];
+// $THEME->sheets = [];
 $THEME->editor_sheets = [];
 $THEME->editor_scss = ['editor'];
 // $THEME->usefallback = true;
-// $THEME->scss = "./preset/remui"; // disabled: scssphp fails on imports, use precompiled CSS
+$THEME->scss = "./preset/remui";
 // $THEME->scss = function($theme) {
 //     return theme_remui_get_main_scss_content($theme);
 // };
